f247775a545bb27a5a013633c56fdf711b405f37
Bernat Borrás Paronella <bernat.borras@privalia.com>
2018-07-03 16:52:46 +0200

    Initial project commit

---
+++ src/main/kotlin/com/alorma/potter/BookCalculator.kt
@@ -0,0 +1,4 @@
+package com.alorma.potter
+
+class BookCalculator {
+}
---
+++ src/test/kotlin/com/alorma/potter/BookCalculatorTest.kt
@@ -0,0 +1,18 @@
+package com.alorma.potter
+
+import org.junit.Before
+import org.junit.Test
+
+class BookCalculatorTest {
+    private lateinit var bookCalculator: BookCalculator
+
+    @Before
+    fun setup() {
+        bookCalculator = BookCalculator()
+    }
+
+    @Test
+    fun `a a`() {
+
+    }
+}
